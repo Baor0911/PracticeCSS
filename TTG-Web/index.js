e31b87a3ff48648aa304d1041cf03af53a65ec58
Baor0911 <128237034+Baor0911@users.noreply.github.com>
2024-05-27 23:10:27 +0700

    Add files via upload

---
+++ TTG-Web/index.js
@@ -0,0 +1,13 @@
+document.querySelector(".mobile-button").addEventListener("click", function () {
+    document.querySelector(".menu").classList.toggle("active");
+  });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const image = document.querySelector(".picture1 img");
+    image.classList.add("show");
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const image = document.querySelector(".picture2 img");
+    image.classList.add("show");
+});
